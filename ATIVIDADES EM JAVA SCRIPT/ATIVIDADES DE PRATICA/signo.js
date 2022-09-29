0bb1d14461496780269bb127f24985db0fd8bf59
GabrielSouzaArcane <97127430+GabrielSouzaArcane@users.noreply.github.com>
2022-09-29 14:13:00 -0300

    commit

---
+++ ATIVIDADES EM JAVA SCRIPT/ATIVIDADES DE PRATICA/signo.js
@@ -0,0 +1,18 @@
+
+ var nome = parseInt(prompt("Seu nome")) 
+ var dia =  parseInt(prompt("Dia que você nasceu"))
+ var mes =  parseInt(prompt("Mês que você nasceu")) 
+
+
+ if (dia >=22 && mes == 12 || dia >=20 && mes == 1) {document.write("Capricórnio")}
+ else if (dia >=21 && mes == 1 || dia >= 19 && mes == 2 ) {document.write("Aquário")}
+ else if (dia >=20 && mes == 2 || dia >= 20 && mes == 3 ) {document.write("Peixe")}
+ else if (dia >=21 && mes == 3 || dia >= 20 && mes == 4 ) {document.write("Áries")}
+ else if (dia >=21 && mes == 4 || dia >= 20 && mes == 5) {document.write("Touro")}
+ else if (dia >=21 && mes == 5 || dia >= 20 && mes == 6 ) {document.write("Gêmeos")}
+ else if (dia >=21 && mes == 6 || dia >= 21 && mes == 7 ) {document.write("Câncer")}
+ else if (dia >=22 && mes == 7 || dia >= 22 && mes == 8 ) {document.write("Leão")}
+ else if (dia >=23 && mes == 8 || dia >= 22 && mes == 9 ) {document.write("Virgem")}
+ else if (dia >=23 && mes == 9 || dia >= 22 && mes == 10 ) {document.write("Libra")}
+ else if (dia >=23 && mes == 10 || dia >= 21 && mes == 11 ) {document.write("Escorpião")}
+ else if (dia >=22 && mes == 11 || dia >= 21 && mes == 12 ) {document.write("Sagitário")}
